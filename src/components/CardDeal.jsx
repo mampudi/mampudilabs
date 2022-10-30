5408b6a7db51017c7693092b7a3e44aca0929760
mampudi <eichlefoko@gmail.com>
2022-10-30 18:08:19 +0200

    add mampudi labs website

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,13 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+    </div>
+
+  </section>
+);
+
+export default CardDeal;
---
+++ src/constants/index.js
@@ -0,0 +1,115 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "",
+    title: "",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Move from idea to market, faster",
+    content:
+      "Eliminate operational overhead so your teams can release quickly, get feedback, and iterate to get to market faster.",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "Lower your costs",
+    content:
+      "With a pay-for-value billing model, resource utilization is automatically optimized and you never pay for over-provisioning.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Adapt at scale",
+    content:
+      "With technologies that automatically scale from zero to peak demands, you can adapt to customer needs faster than ever.",
+  },
+];
+
+export const feedback = [
+
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Serverless Development on AWS",
+    value: "",
+  },
+  {
+    id: "stats-2",
+    title: "Serverless Development on AWS",
+    value: "",
+  },
+  {
+    id: "stats-3",
+    title: "Blockchain Development",
+    value: "",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Dytelligence IT Solutions",
+        link: "https://www.dytelligence.co.za",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Dytelligence IT Solutions",
+        link: "https://www.dytelligence.co.za",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Dytelligence IT Solutions",
+        link: "https://www.dytelligence.co.za",
+      },
+      {
+        name: "Afro Lynx",
+        link: "https://www.afrolynx.co.za/",
+      },
+      {
+        name: "Isanqa",
+        link: "https://www.isanqa.co.za/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
